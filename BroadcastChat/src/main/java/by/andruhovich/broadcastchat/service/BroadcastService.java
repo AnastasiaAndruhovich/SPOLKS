b99ab9809ae4ce6efc67e837f881a6f32a030961
anast <anastasia.andruhovich@gmail.com>
2018-12-06 20:59:19 +0300

    Refactor exception cases

--- BroadcastChat/src/main/java/by/andruhovich/broadcastchat/service/BroadcastService.java
+++ BroadcastChat/src/main/java/by/andruhovich/broadcastchat/service/BroadcastService.java
@@ -9,38 +9,45 @@ import by.andruhovich.broadcastchat.type.CommandType;
 
 public class BroadcastService {
     public void service() {
+        BroadcastListener broadcastListener = null;
+        BroadcastSender broadcastSender;
         boolean running = true;
+
         try {
-            BroadcastListener broadcastListener = new BroadcastListener();
-            BroadcastSender broadcastSender = new BroadcastSender();
+            broadcastListener = new BroadcastListener();
+            broadcastSender = new BroadcastSender();
             broadcastListener.start();
             while (running) {
                 ConsoleWriter.printLine("0 - exit\n1 - print address\nother - message");
                 String line = ConsoleReader.getLine();
                 CommandType commandType = defineCommandType(line);
-                switch (commandType) {
-                    case PRINT:
-                        broadcastSender.printAllBroadcastAddresses();
-                        break;
-                    case MESSAGE:
-                        broadcastSender.sendMessage(line);
-                        break;
-                    case EXIT:
-                        ConsoleReader.close();
-                        broadcastSender.close();
-                        broadcastListener.interrupt();
-                        running = false;
-                        break;
-                    default:
-                        ConsoleWriter.printLine("Incorrect input!");
+                try {
+                    switch (commandType) {
+                        case PRINT:
+                            broadcastSender.printAllBroadcastAddresses();
+                            break;
+                        case MESSAGE:
+                            broadcastSender.sendMessage(line);
+                            break;
+                        case EXIT:
+                            ConsoleReader.close();
+                            broadcastSender.close();
+                            broadcastListener.interrupt();
+                            running = false;
+                            break;
+                        default:
+                            ConsoleWriter.printLine("Incorrect input!");
+                    }
+                } catch (SocketTechnicalException | SendDataTechnicalException e) {
+                    ConsoleWriter.printLine(e.getMessage());
                 }
             }
         } catch (CreateSocketTechnicalException e) {
             ConsoleWriter.printLine(e.getMessage());
-        } catch (SocketTechnicalException e) {
-            ConsoleWriter.printLine(e.getMessage());
-        } catch (SendDataTechnicalException e) {
-            ConsoleWriter.printLine(e.getMessage());
+            ConsoleReader.close();
+            if (broadcastListener != null) {
+                broadcastListener.interrupt();
+            }
         }
     }
 
